d922d945a25b886ba84a6f521c45a320a7984df3
Ibrahim Cherif <cherifibrahim046@gmail.com>
2021-12-18 07:30:32 +0100

    initialisation

---
+++ My-new-google-maps/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My-new-google-maps';
+  
+  lat = 13;
+  lng = 80;
+}
