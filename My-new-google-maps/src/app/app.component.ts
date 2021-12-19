d401d5d6436cf84507ef61ce33caa3b81a459aff
Ibrahim Cherif <cherifibrahim046@gmail.com>
2021-12-19 13:20:28 +0100

    ajout de packages

--- My-new-google-maps/src/app/app.component.ts
+++ My-new-google-maps/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'My-new-google-maps';
-  
-  lat = 13;
-  lng = 80;
+export class AppComponent implements OnInit {
+    zoom = 12.
+    center: google.maps.LatLngLiteral
+    options: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    zoomControl: true,
+    scrollwheel: true,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  }
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+    })
+  }
+
+ 
 }
